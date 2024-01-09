6356be435d62de53cbc5e59c0517cd5f8c1b3190
Daniel Truong <aduy1122@gmail.com>
2024-01-08 16:12:03 -0800

    frontend

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html className="" lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   )
